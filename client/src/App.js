abf2576965a892699b0bfb6ddaaf9eb61c48b5ba
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2020-02-15 17:41:07 -0500

    Add routing to frondend, 2 routes: homepage and suspect

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import MainPage from "./Pages/MainPage/MainPage";
+import SuspectPage from "./Pages/SuspectPage/SuspectPage";
 
 import Sentiment from "./Components/Sentiment/Sentiment.js";
 
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Suspect Analysis App</h1>
-      <Sentiment />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route exact path="/suspect">
+            <SuspectPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/Pages/MainPage/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="MainPage">
+        <h1>Suspect Analysis</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
