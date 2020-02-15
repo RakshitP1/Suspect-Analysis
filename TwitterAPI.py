211dcf59b76d546bf876f98ab542e884732f8087
Nihal Potdar <Nihalpot2002@gmail.com>
2020-02-15 16:01:44 -0500

    mongo db twitter working

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -17,35 +17,38 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth) 
 
 
-client=MongoClient()
-db=client.tweet_db
-tweet_collection=db.tweet_collection
-tweet_collection.create_index([("id",pymongo.ASCENDING)],unique=True)
+#mongo client
+client = MongoClient("mongodb+srv://test:Test123@cluster0-6occj.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
+#get the database
+Users = client.get_database('Twitter_Users')
 
+#get the collection 
+feeds = Users.Twitter_Feeds
+
+#feeds.create_index([("id",pymongo.ASCENDING)],unique=True)
 
 name = "NihalPotdar"
 tweetCount = 10
 
-
+# collection of last 10 tweets
 results = api.user_timeline(id=name, count=tweetCount)
 
 search_results = {}
 
+#iterating through all the tweets
 for tweet in results:
    if name not in search_results:
        search_results[name] = []
    if tweet.text not in search_results[name]:
        search_results[name].append(tweet.text)   
 
-
-
 try:
-    tweet_collection.insert(search_results)
+    feeds.insert(search_results)
 except:
     pass
               
-tweet_cursor = tweet_collection.find()
+tweet_cursor = feeds.find()
 
 for document in tweet_cursor:
    for i in range(len(search_results[name])):
