e364d42fdb3e69345d7f8ef903c8f16c31988464
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2020-02-15 23:00:35 -0500

    Redirect MainPage to SuspectPage with props

--- client/src/Pages/SuspectPage/SuspectPage.js
+++ client/src/Pages/SuspectPage/SuspectPage.js
@@ -1,13 +1,32 @@
 import React from "react";
 
+import queryString from "query-string";
+import { withRouter } from "react-router-dom";
+
 class SuspectPage extends React.Component {
   constructor(props) {
     super(props);
+    // console.log(props);
+    // console.log(this.props);
+    this.state = {
+      name: this.props.location.state.name,
+      twitterHandle: this.props.location.state.twitterHandle
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.props);
   }
 
   render() {
-    return <div className="SuspectPage">Suspect Page</div>;
+    return (
+      <div className="SuspectPage">
+        <h1>Suspect Page</h1>
+        <p>name: {this.state.name}</p>
+        <p>twitter handle: {this.state.twitterHandle}</p>
+      </div>
+    );
   }
 }
 
-export default SuspectPage;
+export default withRouter(SuspectPage);
